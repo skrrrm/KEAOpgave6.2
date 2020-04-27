25e3b51269675fb794ee3c632a2a2151e392508e
skrrrm <a@macbook-windows>
2020-04-27 11:07:56 +0200

    Add project files.

---
+++ KEAOpgave6.2/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace KEAOpgave6._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxNames = new System.Windows.Forms.ListBox();
+            this.buttonCheck = new System.Windows.Forms.Button();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBoxNames
+            // 
+            this.listBoxNames.FormattingEnabled = true;
+            this.listBoxNames.ItemHeight = 25;
+            this.listBoxNames.Location = new System.Drawing.Point(59, 45);
+            this.listBoxNames.Name = "listBoxNames";
+            this.listBoxNames.Size = new System.Drawing.Size(219, 354);
+            this.listBoxNames.TabIndex = 0;
+            // 
+            // buttonCheck
+            // 
+            this.buttonCheck.Location = new System.Drawing.Point(330, 349);
+            this.buttonCheck.Name = "buttonCheck";
+            this.buttonCheck.Size = new System.Drawing.Size(211, 50);
+            this.buttonCheck.TabIndex = 2;
+            this.buttonCheck.Text = "Check";
+            this.buttonCheck.UseVisualStyleBackColor = true;
+            this.buttonCheck.Click += new System.EventHandler(this.buttonCheck_Click);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(330, 207);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(211, 31);
+            this.textBoxName.TabIndex = 3;
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(325, 164);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(0, 25);
+            this.labelInfo.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 450);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.buttonCheck);
+            this.Controls.Add(this.listBoxNames);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxNames;
+        private System.Windows.Forms.Button buttonCheck;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelInfo;
+    }
+}
+
---
+++ KEAOpgave6.2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KEAOpgave6._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            labelInfo.Text = "Write a name, and we'll check if we have it.";
+        }
+
+        private void buttonCheck_Click(object sender, EventArgs e)
+        {
+            // one way to initialize array
+            //string[] firstNames = new string[10];
+
+            // adding names to array
+            //firstNames[0] = "Lone";
+            //firstNames[1] = "Lene";
+            //firstNames[2] = "Peter";
+            //firstNames[3] = "Hans";
+            //firstNames[4] = "Karl";
+            //firstNames[5] = "Børge";
+            //firstNames[6] = "Egon";
+            //firstNames[7] = "Bente";
+            //firstNames[8] = "Linda";
+            //firstNames[9] = "Gerda";
+
+            // another way to initialize arrays
+            string[] firstNames = { "Lone", "Lene", "Peter", "Hans", "Karl", "Børge", "Egon", "Bente", "Linda", "Gerda" };
+
+            for (int i = 0; i < firstNames.Length; i++) // going through firstNames array
+            {
+                if (firstNames[i] == textBoxName.Text.Trim()) // if input fits with an entry in firstNames
+                {
+                    labelInfo.Text = "We do have the name!"; // prints to label
+                }
+            }
+
+        }
+    }
+}
