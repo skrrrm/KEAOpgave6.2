1d08b0ed226ccaec0c980b5038219d1e1f3422b1
skrrrm <a@macbook-windows>
2020-04-27 11:10:39 +0200

    uncommented

--- KEAOpgave6.2/Form1.cs
+++ KEAOpgave6.2/Form1.cs
@@ -21,31 +21,32 @@ namespace KEAOpgave6._2
         private void buttonCheck_Click(object sender, EventArgs e)
         {
             // one way to initialize array
-            //string[] firstNames = new string[10];
+            string[] firstNames1 = new string[10];
 
             // adding names to array
-            //firstNames[0] = "Lone";
-            //firstNames[1] = "Lene";
-            //firstNames[2] = "Peter";
-            //firstNames[3] = "Hans";
-            //firstNames[4] = "Karl";
-            //firstNames[5] = "Børge";
-            //firstNames[6] = "Egon";
-            //firstNames[7] = "Bente";
-            //firstNames[8] = "Linda";
-            //firstNames[9] = "Gerda";
+            firstNames1[0] = "Lone";
+            firstNames1[1] = "Lene";
+            firstNames1[2] = "Peter";
+            firstNames1[3] = "Hans";
+            firstNames1[4] = "Karl";
+            firstNames1[5] = "Børge";
+            firstNames1[6] = "Egon";
+            firstNames1[7] = "Bente";
+            firstNames1[8] = "Linda";
+            firstNames1[9] = "Gerda";
 
             // another way to initialize arrays
             string[] firstNames = { "Lone", "Lene", "Peter", "Hans", "Karl", "Børge", "Egon", "Bente", "Linda", "Gerda" };
 
-            for (int i = 0; i < firstNames.Length; i++) // going through firstNames array
+            // going through firstNames array
+            for (int i = 0; i < firstNames.Length; i++)
             {
-                if (firstNames[i] == textBoxName.Text.Trim()) // if input fits with an entry in firstNames
+                // if input fits with an entry in firstNames, also trims input of spaces
+                if (firstNames[i] == textBoxName.Text.Trim())
                 {
                     labelInfo.Text = "We do have the name!"; // prints to label
                 }
             }
-
         }
     }
 }
